01e5a7798e42d3426c52d4d90e10ef2e3e91df3b
Bellec Erwan <auto-entrepreneur@bellecerwan.com>
2017-11-07 18:08:31 +0100

    get quotes and characters form internet
    ajout des fichiers *_scrpy.py qui permettent de récupérer des citations et personnages depuis internet en utilisant la dépendance "scrapy"

--- san_antonio.py
+++ san_antonio.py
@@ -2,6 +2,7 @@
 import random
 import json
 
+# Give a Json file and return a List
 def read_values_from_json(file, key):
 	values = []
 	with open(file) as f:
@@ -9,6 +10,17 @@ def read_values_from_json(file, key):
 		for entry in data:
 			values.append(entry[key])
 	return values
+
+# Give a json and return a list
+def clean_strings(sentences):
+	cleaned = []
+	# Store quotes on a list. Create an empty list and add each sentence one by one.
+	for sentence in sentences:
+		# Clean quotes from whitespace and so on
+		clean_sentence = sentence.strip()
+		# don't use extend as it adds each letter one by one!
+		cleaned.append(clean_sentence)
+	return cleaned
 
 # Get message 
 def message(character, quote):
@@ -22,17 +34,25 @@ def get_random_item(my_list):
 	random_number = random.randint(0, len(my_list) - 1)
 	return my_list[random_number]
 
+# Return a random value from a json file
+def random_value(source_path, key):
+	all_values = read_values_from_json(source_path, key)
+	clean_values = clean_strings(all_values)
+	return get_random_item(clean_values)
+
 # get user response
 user_response = input("Press enter to know the next quote or B for close a current program").capitalize()
 
 while user_response != "B":
-	characters = read_values_from_json('characters.json', 'character')
-	quotes = read_values_from_json('quotes.json', 'quote')
+	characters = random_value('characters.json', 'character')
+	quotes = random_value('quotes.json', 'quote')
 
+	# Print the message using a random line from the list
 	print(
-		message( get_random_item(characters), get_random_item(quotes) )
+		message( characters, quotes )
 	)
 
+	# get user response
 	user_response = input("Press enter to know the next quote or B for close a current program").capitalize()
 
 if user_response == "B":
